24b454109450f428231dd6a1c955fbeeb9276572
AlxInc <108522772+AlxInc@users.noreply.github.com>
2024-10-28 21:56:15 +1030

    started tileset

---
+++ tileset.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset" tilewidth="128" tileheight="256" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="4">
+  <image width="64" height="64" source="sprites/hedge.png"/>
+ </tile>
+ <tile id="1">
+  <image width="36" height="36" source="sprites/edgeleft.png"/>
+ </tile>
+ <tile id="2">
+  <image width="36" height="36" source="sprites/ground.png"/>
+ </tile>
+ <tile id="3">
+  <image width="36" height="36" source="sprites/edgeright.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="sprites/bone.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="sprites/bench.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="sprites/bin.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="sprites/tree.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="256" source="sprites/lamp.png"/>
+ </tile>
+</tileset>
