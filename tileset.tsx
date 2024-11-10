a6ccc91a778633474b1ff166e2c3c49607e776ec
victoria.stevens <vic.j.stevens@gmail.com>
2024-11-10 12:01:51 +1030

    added tileset.

--- tileset.tsx
+++ tileset.tsx
@@ -1,31 +1,73 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tileset" tilewidth="128" tileheight="256" tilecount="9" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="tileset" tilewidth="128" tileheight="128" tilecount="23" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="4">
-  <image width="64" height="64" source="sprites/hedge.png"/>
- </tile>
- <tile id="1">
-  <image width="36" height="36" source="sprites/edgeleft.png"/>
+  <image source="sprites/hedge.png" width="64" height="64"/>
  </tile>
  <tile id="2">
-  <image width="36" height="36" source="sprites/ground.png"/>
+  <image source="sprites/ground.png" width="64" height="64"/>
  </tile>
  <tile id="3">
-  <image width="36" height="36" source="sprites/edgeright.png"/>
+  <image source="sprites/edgeright.png" width="64" height="64"/>
  </tile>
  <tile id="6">
-  <image width="64" height="64" source="sprites/bone.png"/>
- </tile>
- <tile id="7">
-  <image width="64" height="64" source="sprites/bench.png"/>
+  <image source="sprites/bone.png" width="64" height="64"/>
  </tile>
  <tile id="8">
-  <image width="64" height="64" source="sprites/bin.png"/>
+  <image source="sprites/bin.png" width="64" height="64"/>
  </tile>
  <tile id="9">
-  <image width="128" height="128" source="sprites/tree.png"/>
+  <image source="sprites/tree.png" width="128" height="128"/>
  </tile>
- <tile id="5">
-  <image width="64" height="256" source="sprites/lamp.png"/>
+ <tile id="10">
+  <image source="sprites/edgeright.png" width="64" height="64"/>
+ </tile>
+ <tile id="12">
+  <image source="sprites/ground.png" width="64" height="64"/>
+ </tile>
+ <tile id="14">
+  <image source="sprites/edgeleft.png" width="64" height="64"/>
+ </tile>
+ <tile id="15">
+  <image source="sprites/lamp.png" width="32" height="128"/>
+ </tile>
+ <tile id="16">
+  <image source="sprites/bench.png" width="64" height="64"/>
+ </tile>
+ <tile id="20">
+  <image source="sprites/water.png" width="64" height="64"/>
+ </tile>
+ <tile id="21">
+  <image source="sprites/water2.png" width="64" height="64"/>
+ </tile>
+ <tile id="22">
+  <image source="sprites/water3.tif" width="64" height="64"/>
+ </tile>
+ <tile id="23">
+  <image source="sprites/fence.png" width="64" height="64"/>
+ </tile>
+ <tile id="27">
+  <image source="sprites/dirt.png" width="64" height="64"/>
+ </tile>
+ <tile id="28">
+  <image source="sprites/dirt_fade.png" width="64" height="64"/>
+ </tile>
+ <tile id="29">
+  <image source="sprites/fish3.png" width="64" height="128"/>
+ </tile>
+ <tile id="30">
+  <image source="sprites/bread.png" width="64" height="64"/>
+ </tile>
+ <tile id="31">
+  <image source="sprites/birdbath.png" width="64" height="64"/>
+ </tile>
+ <tile id="32">
+  <image source="sprites/owl2.png" width="64" height="64"/>
+ </tile>
+ <tile id="33">
+  <image source="sprites/duck.png" width="64" height="64"/>
+ </tile>
+ <tile id="34">
+  <image source="sprites/cat/cat.png" width="64" height="64"/>
  </tile>
 </tileset>
